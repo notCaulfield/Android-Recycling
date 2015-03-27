cb3048593ed3a27b043070db0d3a5b2e4ed546c0
Holden Smith <holdenhas@gmail.com>
2015-03-27 19:54:38 -0400

    Design change and minor refactor
    Moved two java class files into the right package, and commented out the
    old plans for Center type storage (was internal to the class, now based
    externally by an array - to be added in next commit)

---
+++ WasteNot/app/src/main/java/sdu/capstone/wastenot/Center.java
@@ -0,0 +1,75 @@
+package sdu.capstone.wastenot;
+
+/**
+ * Created by Holden on 3/15/2015.
+ */
+public class Center {
+    private long id;
+    private String name = null;
+    private String address = null;
+    private String phone = null;
+    private String fax = null;
+    private String email = null;
+    private String contact = null;
+   /// private String[] types = null; // Storing types by external array Very Important (create separately for safety)
+
+
+    //If an element is not present, null will be in it's place
+    public Center(String name, String address, String phone, String fax, String email, String contact) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.fax = fax;
+        this.email = email;
+        this.contact = contact;
+    }
+
+    //Returns String Array
+    /*public String[] getTypes() {
+        return types;
+    }
+
+    //Takes in String Array containing types of materials accepted
+    public void setType(String[] str) {
+        types = str;
+    }*/
+
+    //Once made we only need to get values, not change them - thus only getters
+    //If a getter returns null, then do not display for that value
+
+    public String getContact() {
+
+        return contact;
+    }
+
+    public String getEmail() {
+
+        return email;
+    }
+
+    public String getFax() {
+
+        return fax;
+    }
+
+    public String getPhone() {
+
+        return phone;
+    }
+
+    public String getAddress() {
+
+        return address;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public long getId() {
+
+        return id;
+    }
+
+}
