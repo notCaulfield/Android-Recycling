92e1143ad4579cfeba850bece517d2ecf85051a6
spectra96 <alphonse.koffi13@gmail.com>
2015-03-31 22:35:31 -0400

    This is me adding elemntes the spinner and beginning the implementation of the ListView

---
+++ WasteNot/app/src/main/java/sdu/capstone/wastenot/CustomAdapter.java
@@ -0,0 +1,69 @@
+package sdu.capstone.wastenot;
+
+
+import java.util.ArrayList;
+
+import com.publicstaticdroidmain.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class MyCustomBaseAdapter extends BaseAdapter {
+    private static ArrayList<Center> searchArrayList;
+
+    private LayoutInflater mInflater;
+
+    public MyCustomBaseAdapter(Context context, ArrayList<Center> results) {
+        searchArrayList = results;
+        mInflater = LayoutInflater.from(context);
+    }
+
+    public int getCount() {
+        return searchArrayList.size();
+    }
+
+    public Object getItem(int position) {
+        return searchArrayList.get(position);
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.custom_row_view, null);
+            holder = new ViewHolder();
+            holder.txtName = (TextView) convertView.findViewById(R.id.name);
+            holder.txtAdress = (TextView) convertView
+                    .findViewById(R.id.address);
+            holder.txtPhone = (TextView) convertView.findViewById(R.id.phone);
+            holder.txtWebsite = (TextView) convertView.findViewById(R.id.website);
+            holder.txtEmail = (TextView) convertView.findViewById(R.id.email);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.txtName.setText(searchArrayList.get(position).getName());
+        holder.txtAdress.setText(searchArrayList.get(position)
+                .getAdress());
+        holder.txtPhone.setText(searchArrayList.get(position).getPhone());
+        holder.txtWebsite.setText(searchArrayList.get(position).getWebsite());
+        holder.txtEmail.setText(searchArrayList.get(position).getEmail());
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView txtName;
+        TextView txtAdress;
+        TextView txtPhone;
+        TextView txtWebsite;
+        TextView txtEmail;
+    }
+}
--- WasteNot/app/src/main/java/sdu/capstone/wastenot/MainActivity.java
+++ WasteNot/app/src/main/java/sdu/capstone/wastenot/MainActivity.java
@@ -6,49 +6,53 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Spinner;
 import android.widget.ArrayAdapter;
-
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
     private Spinner list;
     private DataStorage data = new DataStorage();
-
+ArrayList <Center> centers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        data.getType("shingles")[0].getContact();
+        centers= data.getType("shingles");
+
+        final ListView lv = (ListView) findViewById(R.id.rcListView);
+        lv.setAdapter(new MyCustomBaseAdapter(this, searchResults));
         initSpinner();
+
     }
 
     public void initSpinner() {
         list = (Spinner) findViewById(R.id.list_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.hardcoded_recycling, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        list.setAdapter(adapter);
-        list.setOnItemSelectedListener(new SpinnerListener());
-    }
+                R.array.recycle_array, android.R.layout.simple_spinner_item);
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            // Apply the adapter to the spinner
+            list.setAdapter(adapter);
+            list.setOnItemSelectedListener(new SpinnerListener());
+        }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+            return true;
+        }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
             return true;
         }
 
