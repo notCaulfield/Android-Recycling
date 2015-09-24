1d65f40f94cfc16aca3cb66b5a55c6ae94090f24
Holden Smith <holdenhas@gmail.com>
2015-09-24 19:22:02 -0400

    Added "Documentation" and a MINOR Change
    Per my brother's request/observation, the 3-Dots and empty "Settings" on
    the MainActivity have been removed.
    
    You r Welcome (But really, good call, thanks)
    
    Oh, and I added some text to the README file. Probably a good thing to
    do.

--- WasteNot/app/src/main/java/sdu/capstone/wastenot/MainActivity.java
+++ WasteNot/app/src/main/java/sdu/capstone/wastenot/MainActivity.java
@@ -100,6 +100,7 @@ public class MainActivity extends ActionBarActivity implements GoogleApiClient.C
         });
         }
 
+    /*
         @Override
         public boolean onCreateOptionsMenu(Menu menu) {
             // Inflate the menu; this adds items to the action bar if it is present.
@@ -121,7 +122,7 @@ public class MainActivity extends ActionBarActivity implements GoogleApiClient.C
 
         return super.onOptionsItemSelected(item);
     }
-
+*/
     protected synchronized void buildGoogleApiClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
